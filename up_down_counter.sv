ce3cf1a0e70b340e1412212a0e3d408c18a4d5c1
c-coh <119373344+c-coh@users.noreply.github.com>
2022-11-28 21:40:14 -0500

    Update up_down_counter.sv

--- up_down_counter.sv
+++ up_down_counter.sv
@@ -9,20 +9,20 @@ parameter N = 4
   output logic [N-1:0] q,
   output logic rco_b
   );
-  always_ff @(posedge clk) begin
-  if (~en_b) begin
-  if (load_b) begin
-  if(up) begin
-  q = (q + 1);
-  end
-  else begin
-  q = (q - 1);
-  end
-  end
-  else begin
-  q <= load_in;
-  end
-  end
-  end
-  assign rco_b = ~((&q & up) + (~(|q) & ~up));
+    always_ff @(posedge clk) begin
+      if (~en_b) begin
+        if (load_b) begin
+          if(up) begin
+            q = (q + 1);
+          end
+          else begin
+            q = (q - 1);
+          end
+        end
+        else begin
+          q <= load_in;
+        end
+       end
+     end
+    assign rco_b = ~((&q & up) + (~(|q) & ~up));
 endmodule
