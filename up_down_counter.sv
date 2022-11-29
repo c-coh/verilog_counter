aee4fcba96f6789233837d7e24fc531b164fd1dd
c-coh <119373344+c-coh@users.noreply.github.com>
2022-11-28 20:02:02 -0500

    Create up_down_counter.sv

---
+++ up_down_counter.sv
@@ -0,0 +1,28 @@
+module up_down_counter #(
+parameter N = 4
+) (
+  input logic clk,
+  input logic en_b,
+  input logic load_b,
+  input logic up,
+  input logic [N-1:0] load_in,
+  output logic [N-1:0] q,
+  output logic rco_b
+  );
+  always_ff @(posedge clk) begin
+  if (~en_b) begin
+  if (load_b) begin
+  if(up) begin
+  q = (q + 1);
+  end
+  else begin
+  q = (q - 1);
+  end
+  end
+  else begin
+  q <= load_in;
+  end
+  end
+  end
+  assign rco_b = ~((&q & up) + (~(|q) & ~up));
+endmodule
