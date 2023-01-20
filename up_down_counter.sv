d0c9f6aaf5bcf5e18c20ac7c65e1e823102462a5
c-coh <119373344+c-coh@users.noreply.github.com>
2023-01-19 21:28:57 -0500

    Update up_down_counter.sv

--- up_down_counter.sv
+++ up_down_counter.sv
@@ -1,17 +1,18 @@
 module up_down_counter #(
 parameter N = 4
 ) (
-  input logic clk,
-  input logic en_b,
-  input logic load_b,
+  input logic clock,
   input logic up,
-  input logic [N-1:0] load_in,
-  output logic [N-1:0] q,
-  output logic rco_b
+  input logic set_b,
+  input logic enable_b,
+  input logic [N-1:0] set,
+  output logic carry_b,
+  output logic [N-1:0] q
+  
   );
-    always_ff @(posedge clk) begin
-      if (~en_b) begin
-        if (load_b) begin
+  always_ff @(posedge clock) begin
+    if (~en_b) begin
+      if (set_b) begin
           if(up) begin
             q = (q + 1);
           end
@@ -20,9 +21,9 @@ parameter N = 4
           end
         end
         else begin
-          q <= load_in;
+          q <= set;
         end
        end
      end
-    assign rco_b = ~((&q & up) + (~(|q) & ~up));
+    assign carry_b = ~((&q & up) + (~(|q) & ~up));
 endmodule
